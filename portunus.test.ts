451d4c1fed937a72da5628e3dd72a6c25675d706
Alex Olson <alex.olson@gis-ag.com>
2019-05-08 00:07:01 +0200

    more & better tests.

--- portunus.test.ts
+++ portunus.test.ts
@@ -1,21 +1,39 @@
 import { portunus } from "./portunus";
 
 describe('portunus', () => {
-  it('should return 254 when given CCLIV', () => {
-    expect(portunus('CCLIV').convertedValue).toBe(254);
-  });
+  const numeralToDecimalIOMap = {
+    'CCLIV': 254,
+    'CCLV': 255,
+    'MMCDXXI': 2421,
+    'XXXIX': 39,
+    'CCXLVI': 246,
+    'CLX': 160,
+    'CCVII': 207,
+    'MLXVI': 1066,
+    'MDCCLXXVI': 1776,
+    'MCMLIV': 1954,
+    'MCMXC': 1990,
+    'MMXIV': 2014,
+    'MMXIX': 2019,
+  };
 
-  it('should return 255 when given CCLV', () => {
-    expect(portunus('CCLV').convertedValue).toBe(255);
-  });
+  const decimalToNumeralIOMap = Object.keys(numeralToDecimalIOMap)
+    .reduce((acc, currentValue) => {
+      return {
+        [numeralToDecimalIOMap[currentValue]]: currentValue,
+        ...acc,
+      };
+    }, {});
 
-  it('should return 2421 when given MMCDXXI', () => {
-    expect(portunus('MMCDXXI').convertedValue).toBe(2421);
-  });
+  for (const [numeral, decimal] of Object.entries(numeralToDecimalIOMap)) {
+    it(`should return ${decimal} when given ${numeral}`, () => {
+      expect(portunus(numeral).convertedValue).toBe(decimal);
+    });
+  }
 
-  it('should return an empty string when given a number', () => {
-    portunus(200);
-    portunus(5293);
-    expect(portunus(0).convertedValue).toBe('');
-  });
+  for (const [decimal, numeral] of Object.entries(decimalToNumeralIOMap)) {
+    it(`should return ${numeral} when given ${decimal}`, () => {
+      expect(portunus(decimal).convertedValue).toBe(numeral);
+    });
+  }
 });
