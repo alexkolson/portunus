11e41ec5002a68816e1a9324e3485d2a75bf6d7d
Alex Olson <alex.olson@gis-ag.com>
2019-05-07 16:44:39 +0200

    some more tests.

--- portunus.test.ts
+++ portunus.test.ts
@@ -1,8 +1,11 @@
 import { portunus } from "./portunus";
 
 describe('portunus', () => {
-  it('should return 5', () => {
-    const { convertedValue } = portunus('tacos');
+  it('should return 0 when given a string', () => {
     expect(portunus('tacos').convertedValue).toBe(0);
+  });
+
+  it('should return an empty string when given a number', () => {
+    expect(portunus(0).convertedValue).toBe('');
   });
 });
