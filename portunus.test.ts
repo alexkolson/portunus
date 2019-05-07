a6adde8f69f116f182c20d0680c3e998226e7b39
Alex Olson <alex.olson@gis-ag.com>
2019-05-07 18:04:15 +0200

    another numeral => decimal test. Take test cases from wikipedia probably.

--- portunus.test.ts
+++ portunus.test.ts
@@ -9,6 +9,10 @@ describe('portunus', () => {
     expect(portunus('CCLV').convertedValue).toBe(255);
   });
 
+  it('should return 2421 when given MMCDXXI', () => {
+    expect(portunus('MMCDXXI').convertedValue).toBe(2421);
+  });
+
   it('should return an empty string when given a number', () => {
     portunus(200);
     portunus(5293);
