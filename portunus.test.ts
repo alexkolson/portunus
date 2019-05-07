780160c1fc4965467469bb767ba4ff802b75a948
Alex Olson <alex.olson@gis-ag.com>
2019-05-07 17:52:28 +0200

    first test for numeral to decimal.

--- portunus.test.ts
+++ portunus.test.ts
@@ -1,8 +1,8 @@
 import { portunus } from "./portunus";
 
 describe('portunus', () => {
-  it('should return 0 when given a string', () => {
-    expect(portunus('tacos').convertedValue).toBe(0);
+  it('should return 254 when given CCLIV', () => {
+    expect(portunus('CCLIV').convertedValue).toBe(254);
   });
 
   it('should return an empty string when given a number', () => {
